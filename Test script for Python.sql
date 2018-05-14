2e99eff56265193e9bb06a651df63cc0ef6c8596
unknown <nosull@yahoo.com>
2018-05-13 18:54:08 -0700

    added test file for python

---
+++ Test script for Python.sql
@@ -0,0 +1,7 @@
+EXEC sp_execute_external_script @language = N'Python',
+@script=N'
+OutputDataSet = InputDataSet;
+',
+@input_data_1 =N'SELECT 1 AS hello'
+WITH RESULT SETS (([hello] int not null));
+GO
