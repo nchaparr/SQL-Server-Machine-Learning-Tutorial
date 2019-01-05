c08cfc2005b7e95de5bfb2a8b6d81f64e2b5fd82
unknown <nosull@yahoo.com>
2019-01-04 19:59:15 -0800

    Finished linear model

--- Predictive-Model.sql
+++ Predictive-Model.sql
@@ -24,3 +24,32 @@ BEGIN
 END;
 GO
 
+DROP TABLE stopping_distance_models
+CREATE TABLE stopping_distance_models (
+    model_name varchar(20) not null default('default model') primary key,
+    model varbinary(max) not null);
+
+UPDATE stopping_distance_models
+SET model_name = 'rxLinMod ' + format(getdate(), 'yyy.MM.HH.mm', 'en-gb')
+WHERE model_name = 'default model'
+
+INSERT INTO stopping_distance_models (model)
+EXEC generate_linear_model;
+
+DECLARE @model varbinary(max), @modelname varchar(30)
+EXEC sp_execute_external_script
+    @language = N'R'
+    , @script = N'
+        speedmodel <- rxLinMod(distance ~ speed, CarsData)
+        modelbin <- serialize(speedmodel, NULL)
+        OutputDataSet <- data.frame(coefficients(speedmodel));'
+    , @input_data_1 = N'SELECT speed, distance FROM CarSpeed'
+    , @input_data_1_name = N'CarsData'
+    , @params = N'@modelbin varbinary(max) OUTPUT'
+    , @modelbin = @model OUTPUT
+    WITH RESULT SETS (([Coefficient] float not null))
+
+INSERT INTO [dbo].[stopping_distance_models] (model_name, model)
+VALUES ('latest model', @model)
+
+
