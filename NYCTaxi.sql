f28f7abac7a0537fdba26c972f6641952d3c9eec
unknown <nosull@yahoo.com>
2019-08-05 10:41:57 -0700

    Lessons 3 and 4 of NYCTaxi exammple tutorial

--- NYCTaxi.sql
+++ NYCTaxi.sql
@@ -1,9 +1,1 @@
-SELECT TOP(10) * FROM dbo.nyctaxi_sample;
-SELECT COUNT(*) FROM dbo.nyctaxi_sample;
 
-SELECT DISTINCT [passenger_count]
-    , ROUND (SUM ([fare_amount]),0) as TotalFares
-    , ROUND (AVG ([fare_amount]),0) as AvgFares
-FROM [dbo].[nyctaxi_sample]
-GROUP BY [passenger_count]
-ORDER BY  AvgFares DESC
